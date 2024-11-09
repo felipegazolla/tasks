7515e28c8dfcdade4a2e0562d8944ec19d71795d
felipe <enggazolla@gmail.com>
2024-11-09 12:58:34 -0300

    change directory

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
-  
+	const { method, url } = req;
+
+	if (method === "GET" && url === "/tasks") {
+		return res.end("Listagem de tarefas");
+	}
+
+	if (method === "POST" && url === "/tasks") {
+		return res.end("Criação de tarefas");
+	}
+
+	return res.end("Hello, world!");
 });
 
 server.listen(3333);
