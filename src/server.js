3a5326561af80c270033872fc63ca1d8d6040a1f
felipe <enggazolla@gmail.com>
2024-11-09 21:20:11 -0300

    stream

--- src/server.js
+++ src/server.js
@@ -1,17 +1,46 @@
 import http from "node:http";
 
-const server = http.createServer((req, res) => {
+const tasks = [];
+
+const server = http.createServer(async (req, res) => {
 	const { method, url } = req;
 
+	const buffers = [];
+
+	for await (const chunk of req) {
+		buffers.push(chunk);
+	}
+
+	try {
+		req.body = JSON.parse(Buffer.concat(buffers).toString());
+	} catch {
+		req.body = null;
+	}
+
+	console.log(req.body);
+
 	if (method === "GET" && url === "/tasks") {
-		return res.end("Listagem de tarefas");
+		return res
+			.setHeader("Content-Type", "application/json")
+			.end(JSON.stringify(tasks));
 	}
 
 	if (method === "POST" && url === "/tasks") {
-		return res.end("Criação de tarefas");
+		const { title, description } = req.body;
+
+		tasks.push({
+			id: 1,
+			title,
+			description,
+			completed_at: null,
+			created_at: null,
+			updated_at: null,
+		});
+
+		return res.writeHead(201).end("Tarefa Criada!");
 	}
 
-	return res.end("Hello, world!");
+	return res.writeHead(404).end();
 });
 
 server.listen(3333);
